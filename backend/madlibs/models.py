3279e549ee2d67293e5b19f96f4e4406a8af097e
zimmshane <zimmshane@gmail.com>
2025-10-28 15:51:55 -0700

    add filled_content model

--- backend/madlibs/models.py
+++ backend/madlibs/models.py
@@ -1,5 +1,6 @@
 from bson import ObjectId
 from typing import Optional, List, Dict
+from datetime import datetime, timezone
 from core.db_connect import get_collection
 
 
@@ -139,3 +140,96 @@ class MadLibTemplate:
             print(f"Error deleting madlib: {e}")
             return False
 
+class UserFilledMadlibs:
+    @staticmethod
+    def new_filled_madlib(template_id, creator_id, inputted_blanks) -> str:
+        """
+        Add a filled in madlib template to the database
+
+        Args:
+            template_id: String of MongoDB ObjectID to fill in
+            creator_id: String of the user's MongoDB ObjectID 
+            inputted_blanks: dict of inputted content
+            
+        Example of Inputted_blank:
+        [
+            "inputted_blanks":
+                {
+                    "id": "1",
+                    "input": "sing"
+                },
+                {
+                    "id": "2",
+                    "input": "running"
+                },
+                {
+                    "id": "3",
+                    "input": "creepy"
+                },
+                {
+                    "id": "4",
+                    "input": "12"
+                }
+        ]
+        
+        Example of madlib_data:
+        [
+            {
+            "user_oid":{"$oid":"Object ID"},
+            "template_oid":{"$oid":"Object ID"},
+            "created_on": "datetime",
+            "modified_on": "datetime",
+            "public" : true,
+            "inputted_blanks":
+                            {
+                                "id": "1",
+                                "input": "sing"
+                            },
+                            {
+                                "id": "2",
+                                "input": "running"
+                            },
+                            {
+                                "id": "3",
+                                "input": "creepy"
+                            },
+                            {
+                                "id": "4",
+                                "input": "12"
+                            }
+            }
+        ]
+            
+        Returns:
+            The ObjectID of the new db object
+        """
+        filled_madlib_collection = get_collection('filled_madlibs')
+
+        now = datetime.now(timezone.utc)
+        
+        madlib_data = {
+            'template_id': template_id,
+            'creator_id': creator_id,
+            'created_at': now,
+            'updated_at': now,
+            'public': True,
+            'content': inputted_blanks,
+        }
+
+        result = filled_madlib_collection.insert_one(madlib_data)
+        return str(result.inserted_id)
+    
+    @staticmethod
+    def update_filled_madlib(filled_madlib_id,inputted_blanks) -> bool:
+        filled_madlib_collection = get_collection('filled_madlibs')
+        
+        try:
+            result = filled_madlib_collection.update_one(
+                {'_id': ObjectId(filled_madlib_id)},
+                {'$set': inputted_blanks}
+            )
+            return result.modified_count > 0
+        except Exception as e:
+            print(f"Error updating madlib: {e}")
+            return False
+        
