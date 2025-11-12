3d27716cf5e89853b67d8f1018ae0b6b47240c84
apaya22 <andypayan22@gmail.com>
2025-11-11 17:00:03 -0800

    Created API endpoint for handling image uploads to AWS bucket

--- backend/social/views.py
+++ backend/social/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from backend.social.utils import upload_ai_image
+
 
 # Create your views here.
+
+
+
+
+
+
+
+
+
+
+
+
+#api for uploading madlib image to s3
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def upload_madlib_image(request, madlib_id):
+    """
+    Uploads an AI-generated image for a specific Madlib.
+    """
+    file = request.FILES.get('image')
+    if not file:
+        return Response({'error': 'No image file provided.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    url = upload_ai_image(file, madlib_id)
+
+    if url:
+        return Response({'url': url, 'message': 'Image uploaded successfully!'})
+    else:
+        return Response({'error': 'Upload failed'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
