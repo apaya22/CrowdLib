662f9c63100be569234fbe662d68f838c256ade9
apaya22 <andypayan22@gmail.com>
2025-11-11 17:11:12 -0800

    Fixed bug for importing image uploading function

--- backend/social/views.py
+++ backend/social/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status, permissions
-from backend.social.utils import upload_ai_image
+from social.utils import upload_ai_image
 
 
 # Create your views here.
