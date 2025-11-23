bc5d7486326efb8689d804e99c0919147eb73f33
Kosh Dosh <jkibr001@ucr.edu>
2025-11-22 16:19:47 -0800

    updated home page

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,16 @@
 //Local call
 export const BACKEND = "http://localhost:8000";
+
+export function getCSRFToken() {
+  const name = "csrftoken=";
+  const decoded = decodeURIComponent(document.cookie);
+  const parts = decoded.split(";");
+
+  for (let p of parts) {
+    let c = p.trim();
+    if (c.startsWith(name)) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+}
